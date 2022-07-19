2e7c6eaebb83c9f6d4170e19981e2d4794411313
pius <pius.opoku@pakatron.com>
2022-07-19 13:22:42 +0000

    halfway through configurations

--- next.config.js
+++ next.config.js
@@ -3,3 +3,5 @@ module.exports = {
     domains: ['cdn.sanity.io'],
   },
 }
+
+//Token: skxiMkOLpUTNrCIWLckQjWOljnA320qAoVoxqeIHIpS7DGkabtu01MXxbvviDEsdk9hgNvszknPU4laGy1GCojYV9sUkuo6bb4Hwx1OQJO1REipzkmuM21Cd2rnbyEOVGZfriqEFutVOUSeeLGANSx7Zq4f3MsdwbJCsEQri6UfriZqYL9Yl
